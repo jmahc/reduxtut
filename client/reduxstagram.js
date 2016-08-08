494b5468d61cd0ee3660cd830f82d1a2b352c129
Jordan McArdle <jmcardle@valexander.com>
2016-08-08 16:37:46 -0500

    Error handling completed - next step is Redux tools

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -10,6 +10,19 @@ import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 import { Provider } from 'react-redux';
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, {
+    tags: {
+        git_commit: 'asdoghshodig',
+        userLevel: 'editor'
+    }
+}).install();
+
+Raven.captureMessage('Something bad happened!');
+Raven.showReportDialog();
+
 // import react router deps
 import router from './components/Router';
 
